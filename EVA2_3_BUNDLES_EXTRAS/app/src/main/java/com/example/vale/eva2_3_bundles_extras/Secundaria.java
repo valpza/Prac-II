1fd5367d60a07e15bf5052d32d8819a206367b22
Unknown <valeriabere.lopeza@gmail.com>
2018-11-20 20:30:47 -0700

    practica 1
    EVA2_1

---
+++ EVA2_3_BUNDLES_EXTRAS/app/src/main/java/com/example/vale/eva2_3_bundles_extras/Secundaria.java
@@ -0,0 +1,29 @@
+package com.example.vale.eva2_3_bundles_extras;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Secundaria extends AppCompatActivity implements Button.OnClickListener {
+    Button btnAtras;
+    Intent inLeer;
+    EditText editText1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secundaria);
+        editText1 = findViewById(R.id.editText2);
+        inLeer = getIntent();
+        editText1.setText(inLeer.getStringExtra("Mensaje"));
+        btnAtras = findViewById(R.id.btnAtras);
+        btnAtras.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        finish();
+    }
+}
